0a4e08b5941ad4d7573aba70f3dc185c944f81c5
Sonyadream <119162043+Sonyawrz@users.noreply.github.com>
2022-12-03 08:39:14 +0000

    s

--- main.py
+++ main.py
@@ -8,7 +8,30 @@ class Student:
   def say_hello(self):
     print('Meow')
   def to_study(self):
-    print('Meow Meow!')
+    print('Time to study')
     if self.progress < 2:
       self.gladness += 3
-    self.progress 
+    self.progress += 4
+    self.gladness -= 2
+  def to_sleep(self):
+    print('Time to sleep')
+    self.gladness += 3
+  def to_chill(self):
+    print('Rest time')
+    self.gladness += 5
+    self.progress -= 2
+  def is_alive(self):
+    if self.progress < -5:
+      print('You are bad')
+      self.alive = False
+    elif self.gladness <= 0:
+      print('Dead inside')
+      self.alive = False
+    elif self.progress > 50:
+      print('Amazing! You are so smart')
+      self.alive = False
+  def statics(self):
+    print('Gladness: ', self.gladness, 'Progress ',self.progress)
+  def live(self, day):
+    day = "Day " + str(day) + " of " + self.breed + " life"
+    
