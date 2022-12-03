c6cca9aae74e11c71643015e723b6028ea68af5d
Sonyadream <119162043+Sonyawrz@users.noreply.github.com>
2022-12-03 08:02:29 +0000

    progress

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from random import *
-cat Life:
+class Student:
+  def __init__(self, breed):
+    self.breed = breed
+    self.eat = 50
+    self.progress = 10
+    self.alive = True
+  def say_hello(self):
+    print('Meow')
+  def to_study(self):
+    print('Meow Meow!')
+    if self.progress < 2:
+      self.gladness += 3
+    self.progress 
